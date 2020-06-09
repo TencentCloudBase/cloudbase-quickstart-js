0a4e32e96f03ccc9c178d97ca37aca13bbeef3dc
zijiezhou <zijiezhou@tencent.com>
2020-06-09 20:48:49 +0800

    fix(storage): fix storage demo

--- storage/static/main.js
+++ storage/static/main.js
@@ -1,12 +1,12 @@
 // 此处填入你的真实环境 ID
-var envId = 'your-env-id'
+var envId = "your-env-id"
 
 class StorageQuickStarter {
   constructor() {
     // 绑定 dom
-    this.fileButton = document.getElementById('file')
+    this.fileButton = document.getElementById("file")
     // 绑定 dom listener
-    this.fileButton.addEventListener('change', this.handleFileSelect.bind(this), false)
+    this.fileButton.addEventListener("change", this.handleFileSelect.bind(this), false)
 
     // 初始化 CloudBase
     this.app = tcb.init({
@@ -18,8 +18,8 @@ class StorageQuickStarter {
   }
 
   signIn() {
-    const auth = this.app.auth({
-      persistence: 'local'
+    var auth = this.app.auth({
+      persistence: "local"
     })
     if(!auth.hasLoginState()) {
       auth.signInAnonymously().then(() => {
@@ -34,27 +34,27 @@ class StorageQuickStarter {
     e.stopPropagation()
     e.preventDefault()
 
-    const file = e.target.files[0]
-    const name = file.name
+    var file = e.target.files[0]
+    var name = file.name
 
     this.app.uploadFile({
       cloudPath: name,
       filePath: file
     }).then(res => {
       // 云文件ID
-      const fileID = res.fileID
+      var fileID = res.fileID
       // 通过云文件ID 获取 云文件链接
       this.app.getTempFileURL({
         fileList: [fileID]
-      }).then(res => {
-        var file = res.fileList[0]
-        var url = file.tempFileURL
-        document.getElementById('linkbox').innerHTML = '<a href="' +  url + '">' + name + '</a>';
+      }).then(res2 => {
+        var fileObj = res2.fileList[0]
+        var url = fileObj.tempFileURL
+        document.getElementById('linkbox').innerHTML = `<a href=${url}> ${name} </a>`
       })
     })
   }
 }
 
-window.addEventListener('load', function() {
+window.addEventListener("load", function() {
   window.app = new StorageQuickStarter()
 })
